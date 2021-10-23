403cc3a42e4ec41fb9db24daea438702c65cf1c7
WenbinWang-1998 <74896960+WenbinWang-1998@users.noreply.github.com>
2021-10-23 16:35:59 -0400

    Update TwitterAPI.py

--- TwitterAPI.py
+++ TwitterAPI.py
@@ -1,4 +1,4 @@
-#reference:https://drive.google.com/file/d/1dNahyZnwgqwUUdbV5I0u68b8pQADBiWi/view
+#reference:https://developer.twitter.com/en/docs/twitter-api/v1/data-dictionary/object-model/tweet
 
 import tweepy
 import json
@@ -11,7 +11,9 @@ twitter_keys = {
         'access_token_secret': 'Enter access token secret here'
     }
 
+# Get top 200 tweets from the user and save them into myTwitterApi.json and save the non-reply tweets into NotReplies.json
 def get_all_tweets_and_reply(id):
+
     auth = tweepy.OAuthHandler(twitter_keys['consumer_key'], twitter_keys['consumer_secret'])
     auth.set_access_token(twitter_keys['access_token_key'], twitter_keys['access_token_secret'])
     api = tweepy.API(auth)
@@ -27,8 +29,8 @@ def get_all_tweets_and_reply(id):
         json.dump(obj, file, indent=4, sort_keys=True)
     file.close()
 
-    #Get tweets which are reply
-    file=open('Replies','w')
+    #Get tweets which are not reply
+    file=open('NotReplies','w')
     for tweet in tweets:
         if hasattr(tweet, 'in_reply_to_status_id_str'):
             # print('dfafsa',tweet.in_reply_to_status_id_str)
@@ -40,9 +42,9 @@ def get_all_tweets_and_reply(id):
     file.close()
 
 
-
-
 if __name__ == '__main__':
-    #pass the username to the function, get all his/her tweets and get the replies tweets, say @elonmusk
-    get_all_tweets_and_reply("@elonmusk")
+    #pass the username to the function, get all his/her tweets and get the replies
+    # get_all_tweets_and_reply("@elonmusk")
+    get_all_tweets_and_reply("@wwenbinbu")
+
 
